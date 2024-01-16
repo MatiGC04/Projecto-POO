98959f0f4f249e57e644d9f7a4307c10403b299f
Palacios Angel <angeluis.0711@gmail.com>
2024-01-16 15:55:06 -0300

    Cree métodos y atributos de personas
    Falta poder la calcular la edad

--- ClasesYFunciones/persona.cpp
+++ ClasesYFunciones/persona.cpp
@@ -2,7 +2,7 @@
 #include <string>
 using namespace std;
 Persona::Persona(string nom, string ape, string mail, string sex,string tel, 
-				 string loc, int dia, int mes, int anio){
+				 string loc, int dni, int dia, int mes, int anio){
 	nombre = nom;
 	apellido = ape;
 	email = mail;
@@ -12,4 +12,47 @@ Persona::Persona(string nom, string ape, string mail, string sex,string tel,
 	dia_nac = dia;
 	mes_nac = mes;
 	anio_nac = anio;
+	DNI = dni;
+	/*edad = funcion para obtener la fecha actual
+	calcular en base a la fecha actual y el nacimiento la edad
+	*/
 }
+///Desarrollo de los métodos para obtener la información de la persona
+string Persona::ver_nombre(){
+	return nombre;
+}
+string Persona::ver_apellido(){
+	return apellido;
+}
+
+string Persona::ver_email(){
+	return email;
+}
+string Persona::ver_tel(){
+	return telefono;
+}
+string Persona::ver_dir(){
+	return direccion;
+}
+string Persona::ver_loc(){
+	return localidad;
+}
+string Persona::ver_sexo(){
+	return sexo;
+}
+int Persona::ver_anioN(){
+	return anio_nac;
+}
+int Persona::ver_mesN(){
+	return mes_nac;
+}
+int Persona::ver_diaN(){
+	return dia_nac;
+}
+int Persona::ver_edad(){
+	return edad;
+}
+int Persona::ver_DNI(){
+	return DNI;
+}
+
