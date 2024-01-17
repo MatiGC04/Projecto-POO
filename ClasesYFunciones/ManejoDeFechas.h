723c61cc4e44e472baa648635612472832dda88c
Junior Albarenque <juninhoalba@gmail.com>
2024-01-17 00:07:07 -0300

    Actualizacion archivos de Fechas y añadidos a Proyecto
    Se termino de crear funciones para el manejo de fechas y hora, proximamente deben agregarse comentarios y cuidar de no añadir el espacio de trabajo std en el archivo .h

--- ClasesYFunciones/ManejoDeFechas.h
+++ ClasesYFunciones/ManejoDeFechas.h
@@ -2,7 +2,6 @@
 #define MANEJODEFECHAS_H
 #include <ctime>
 #include <iostream>
-using namespace std;
 struct Fecha{
 	int dia;
 	int mes;
@@ -10,9 +9,11 @@ struct Fecha{
 };
 Fecha FechaHoy();
 int DifMeses(Fecha F1,Fecha F2);
-bool ConfirmacionFecha(Fecha F1);
-string Fecha_a_texto(Fecha F1);
-Fecha Texto_a_fecha (string Fecha1);
-bool operator<(Fecha F1, Fecha F2);
-bool operator==(Fecha F1,Fecha F2); 
 bool AnioBiciesto(Fecha F1);
+bool ConfirmacionFecha(Fecha F1);
+std::string Fecha_a_texto(Fecha F1);
+bool operator<(Fecha F1, Fecha F2);
+bool operator==(Fecha F1,Fecha F2);
+
+
+#endif
