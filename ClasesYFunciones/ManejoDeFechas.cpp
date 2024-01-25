77405e4235ebe371a6b0299253bf9baa737052c1
Junior Albarenque <juninhoalba@gmail.com>
2024-01-25 11:56:44 -0300

    Agrego comentarios descriptivos para funciones
    Se agregó comentarios que indican parametros de lasfunciones y los datos que retornan

--- ClasesYFunciones/ManejoDeFechas.cpp
+++ ClasesYFunciones/ManejoDeFechas.cpp
@@ -1,15 +1,22 @@
+/**
+* @file ManejoDeFechas.cpp
+* @brief Implementaciones de funciones para el manejo de las fechas 
+**/
 #include <ManejoDeFechas.h>
 #include <string>
 #include <ctime>
 
 /**
-* Funcion que utiliza el tipo de dato ctime 
-* (tm_year devuelve los anios que pasaron desde 1900 y tm_mon devuelve el mes 
-* actual desde iniciando a contar desde 0) para obtener dia, mes y anio.
+* Utiliza la libreria ctime para obtener la fecha actual (tm_year cuenta a 
+* a partir de 1900 y tm_mon desde 0.
+* La funcion time(nullptr) devuelve una variable time_t que contiene los 
+* segundos desde una epoca(01/01/1969 21:00hs) hasta hoy.
+* La funcion localtime recibe una direccion de una variable time_t y devuelve
+* un puntero a tm (struct que tiene datos como anio, dia, mes y mas).
 **/
 fecha FechaHoy(){
-	time_t hoy= time(0);		///La funcion time(nullptr) devuelve una variable time_t que contiene los segundos desde una epoca(01/01/1969 21:00hs) hasta hoy.
-	tm *time = localtime(&hoy);	///La funcion localtime recibe una direccion de una variable time_t y devuelve un puntero a tm (struct que tiene datos como anio, dia, mes y mas).
+	time_t hoy= time(0);		///
+	tm *time = localtime(&hoy);	///
 	fecha aux;
 	aux.anio= time->tm_year+1900;
 	aux.mes= time->tm_mon+1;
@@ -19,8 +26,12 @@ fecha FechaHoy(){
 }
 
 /**
-* Ingresando dia, mes y anio retorna una variable tipo fecha
-* para poder realizar operaciones con ella.
+* Ingresando dia, mes y anio retorna una variable tipo fecha para poder realizar
+* operaciones con ella.
+* La funcion mktime recibe un puntero a tm y utiliza tm_sec, tm_min, tm_hour, 
+* tm_day, tm_mon y tm_year como datos para devolver una variable.
+* @param dia, mes y año de la fecha
+* @return variable tipo fecha
 **/
 fecha ConvertFecha(int dia, int mes, int anio){
 	time_t hoy= time(0);
@@ -35,21 +46,25 @@ fecha ConvertFecha(int dia, int mes, int anio){
 	Fecha1.dia=dia;
 	Fecha1.anio=anio;
 	Fecha1.mes=mes;
-	Fecha1.time=mktime(F);		///La funcion mktime recibe un puntero a tm y utiliza tm_sec, tm_min, tm_hour, tm_day, tm_mon y tm_year como datos para devolver una variable
-								///tipo time_t con los segundos desde la epoca (01/01/1969 21:00hs) hasta la fecha que almacenaba el puntero tm.
+	Fecha1.time=mktime(F);		
 	return Fecha1;				
 
 }
-
 	
-///Funcion convertir fecha a string
+/**
+* Funcion para convertir fecha a string
+* @param variable tipo fecha
+* @return variable tipo string
+**/	
 std::string Fecha_a_texto(fecha F1){
 	return std::to_string(F1.dia)+"/"+std::to_string(F1.mes)+"/"+std::to_string(F1.anio);
 }
 	
 	
-	
-///Verifica si el anio es biciesto o no
+/**
+* Verifica si el anio es biciesto o no
+* @param variable tipo fecha
+**/	
 bool AnioBiciesto(fecha F1){
 	if(F1.anio%100==0){
 		if(F1.anio%400==0)return true;
@@ -59,11 +74,14 @@ bool AnioBiciesto(fecha F1){
 	return false;
 }
 
-///Confirma que la fecha ingresada es valida
+/**
+* Confirma que la fecha ingresada es valida
+* @param variable tipo fecha
+**/		
 bool ConfirmacionFecha(fecha F1){
 	int DiasFeb=28;
 	if(AnioBiciesto(F1)) DiasFeb=29;
-	if(F1.anio<1900){
+	if(F1.anio>0){
 		return false;
 	}
 	else if(F1.mes<1 or F1.mes>12){
@@ -87,17 +105,27 @@ bool ConfirmacionFecha(fecha F1){
 	}
 	return true;
 }
-	
-///Funcion para obtener la diferencia en dias entre dos fechas
-int DifDias(fecha F1,fecha F2){
-	///Interpretando que la primera fecha ingresada es menor que la segunda fecha
-	///se realiza el siguiente calculo
+
+/**
+* Utiliza la funcion difftime de la libreria ctime que devuelve los segundos de
+* diferencia entre cada fecha, aplicando una conversion se obtinen los dias 
+* y se retorna el valor redondeado hacia abajo.
+* Interpretando que F1<F2
+* @param dos variables tipo fecha
+* @return devuelve un entero, la diferencia de dias entre las fechas
+**/	
+int DifDias(fecha F1, fecha F2){
 	float dif=difftime(F2.time,F1.time);
 	dif= (dif)/(60*60*24);
 	return int(dif);
 }
-	
-///Calcula la fecha de vencimiento del pago abonado
+
+/**
+* Calcula la fecha de vencimiento del pago abonado agregandole 31 dias a la 
+* fecha en la que pagó.
+* @param dato tipo fecha
+* @return dato tipo fecha
+**/	
 fecha FechaVencimiento(fecha F1){
 	F1.time=F1.time+31*24*60*60;
 	tm *vencimiento=localtime(&F1.time);
@@ -107,11 +135,10 @@ fecha FechaVencimiento(fecha F1){
 	return F1;
 }
 
-
-
-
-
-///Implementacion para comparar fechas
+/**
+* Sobrecarga del operador menor 
+* @param datos tipo fecha
+**/	
 bool operator<(fecha F1, fecha F2){
 	if(F1.anio<F2.anio){
 		return true;
@@ -126,8 +153,10 @@ bool operator<(fecha F1, fecha F2){
 }
 
 
-
-///Implementacion para verificar que las fechas son iguales
+/**
+* Sobrecarga del operador igual
+* @param datos tipo fecha
+**/
 bool operator==(fecha F1,fecha F2){
 	if(F1.anio!=F2.anio){
 		return false;
@@ -140,8 +169,12 @@ bool operator==(fecha F1,fecha F2){
 	}
 	return true;
 }
-	
-///Sobrecarga de operador << para mostrar por pantalla variables tipo fecha
+
+/**
+* Sobrecarga del operador de salida para mostrar por pantalla
+* @param dato tipo ostream por referencia y variable tipo fecha.
+* @return dato tipo ostream que muestra el dato tipo fecha formato dd/mm/aaaa
+**/	
 std::ostream &operator<<(std::ostream &o, fecha F1){
 	o<<F1.dia<<"/"<<F1.mes<<"/"<<F1.anio;
 	return o;
--- ClasesYFunciones/ManejoDeFechas.h
+++ ClasesYFunciones/ManejoDeFechas.h
@@ -1,32 +1,47 @@
+/**
+* @file ManejoDeFechas.h
+* @brief Declaraciones que se van a utilizar para el manejo de fechas
+*
+* En este archivo se declaran el tipo de dato "fecha" y las funciones para su 
+* manejo.
+**/
+
 #ifndef MANEJODEFECHAS_H
 #define MANEJODEFECHAS_H
 #include <ctime>
 #include <iostream>
+
+/**
+* @brief Estructura para almacenar fechas y operarlas
+*
+* Esta estructura almacena dia, mes, añio y una variable tipo time_t
+* para poder utilizar las funciones de la libreria ctime
+**/
 struct fecha{
 	int dia;
 	int mes;
 	int anio;
 	std::time_t time;
-	
-	fecha operator=(fecha otra){
-		dia=otra.dia;
-		mes=otra.mes;
-		anio=otra.anio;
-		time=otra.time;
-		return *this;
-	}
 };
+///@brief Funcion que devuelve la fecha de hoy
 fecha FechaHoy();
+///@brief Funcion que recibe dia, mes y añio para devolver variable tipo fecha
 fecha ConvertFecha(int dia, int mes, int anio);
+///@brief Funcion que recibe una fecha y retorna un string en formato dd/mm/aaaa
 std::string FechaTexto(fecha F1);
-
+///@brief Funcion que valida si el año de la fecha recibida es biciesto o no
 bool AnioBiciesto(fecha F1);
+///@brief Funcion que valida la fecha ingresada
 bool ConfirmacionFecha(fecha F1);
+///@brief Funcion que devuelve la diferencia de dias entre dos fechas
 int DifDias(fecha F1, fecha F2);
+///@brief Funcion que agrega un mes a la fecha ingresada
 fecha FechaVencimiento(fecha F1);
-
+///@brief Funcion encargada de validar si una fecha es menor que otra
 bool operator<(fecha F1, fecha F2);
+///@brief Funcion encargada de validar si una fecha es igual a otra
 bool operator==(fecha F1,fecha F2);
+///@brief Funcion encargada de mostrar por pantalla una fecha
 std::ostream &operator<<(std::ostream &o, fecha F1);
 
 
