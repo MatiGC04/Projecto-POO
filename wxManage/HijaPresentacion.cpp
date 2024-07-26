36b449ee284727535b5c5b3dfbaeaa6b2f19ce6d
Junior Albarenque <juninhoalba@gmail.com>
2024-07-26 18:13:30 -0300

    Zpr

--- wxManage/HijaPresentacion.cpp
+++ wxManage/HijaPresentacion.cpp
@@ -1,6 +1,6 @@
 #include "HijaPresentacion.h"
 
-HijaPresentacion::HijaPresentacion(manage *aux, wxWindow *parent) : BasePresentacion(parent),m_manage(aux) {
+HijaPresentacion::HijaPresentacion(manage *aux, wxWindow *parent) : BasePresentacion(parent), m_manage(aux){
 	
 }
 
