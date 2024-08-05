d9c38c58a3c7a29146d2b89d9e827ded3614b34d
Matias G.C. <matigc046@hotmail.com>
2024-08-05 20:40:13 -0300

    Documentacion en HijaPlan .cpp/h

--- wxManage/HijaPlan.h
+++ wxManage/HijaPlan.h
@@ -8,16 +8,28 @@ class HijaPlan : public BasePlan {
 private:
 	manage *m_manage;
 protected:
+	/// Evento para cuando se cambia el tamanio de la ventana
 	void CambiarTamanio( wxSizeEvent& event )  override;
+	/// Evento para cuando se clickea en la etiqueta de una columna
 	void ClickColumna( wxGridEvent& event )  override;
+	/// Evento para el enter en la barra de busqueda
 	void ClickBuscar( wxCommandEvent& event )  override;
+	/// Busca por nombre y apellido en la grilla cuando hacemos click 
+	/// en el boton "Buscar"
 	void EnterBuscar( wxCommandEvent& event )  override;
+	/// Elimina el plan que se haya elegido en el desplegable
 	void ClickBorrarPlan( wxCommandEvent& event )  override;
+	/// Crea nuevo plan y abre una ventana de dialogo para crearlo 
 	void ClickCrearPlan( wxCommandEvent& event )  override;
+	/// Evento para hacer click en guaradr ruina
 	void ClickGuardarRutina( wxCommandEvent& event )  override;
+	/// Evento para hacer click en eliminar
 	void ClickEliminar( wxCommandEvent& event )  override;
+	/// Evento para el click en el boton de agregar
 	void AgregarCouchToPlan( wxCommandEvent& event )  override;
+	/// Evento para hacer click en el precio y poder modificarlo
 	void ClickBotonPrecio( wxCommandEvent& event )  override;
+	/// Evento que permine abrir el desplegable y cambiar de plan
 	void CambioSeleccion( wxCommandEvent& event )  override;
 	
 public:
